e546a8bcd6de938355cbf671fea88af2747a8ae0
chadm9 <chadm_@yahoo.com>
2021-12-15 16:56:27 -0500

    parse text body

--- index.js
+++ index.js
@@ -2,21 +2,18 @@ const sgMail = require('@sendgrid/mail')
 
 sgMail.setApiKey(process.env.OCONEE_SINCLAIR_GUIDE_SERVICES_SENDGRID_KEY)
 
-exports.helloWorld = (req, res) => {
-    const name = req.body?.name ?? req.body?.name ?? null;
-    const email = req.body?.email ?? req.body?.email ?? null;
-    const message = req.body?.message ?? req.body?.message ?? null;
+exports.sendEmail = (req, res) => {
+    const body = JSON.parse(req.body)
 
     const msg = {
         to: `${process.env.EMAIL_TO_ADDRESS}`, // Change to your recipient
         from: `${process.env.EMAIL_FROM_ADDRESS}`, // Change to your verified sender
-        subject: `LakeOconeeSincliarGuidService Inquiry from ${req.body.name}`,
-        text: `Sender Name: ${name}\nSender Email: ${email}\nSender Message: ${message}`,
+        subject: `LakeOconeeSincliarGuidService Inquiry from ${body?.name}`,
+        text: `Sender Name: ${body?.name}\nSender Email: ${body?.email}\nSender Message: ${body?.message}`,
     }
 
     sgMail.send(msg)
         .then((response) => {
-            console.log(JSON.stringify(response))
             res.status(200).send('Email sent.');
         })
         .catch((error) => {
